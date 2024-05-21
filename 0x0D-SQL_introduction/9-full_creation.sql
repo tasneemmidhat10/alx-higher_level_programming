6a47a4c94f3abc978ebbc41424533b2c0f01e68d
root <tasneemmidhat@gmail.com>
2024-05-21 11:06:53 -0700

    First

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,2 @@
 -- Creating and inserting into a table simaltenously 
-INSERT INTO IF EXISTS (CREATE TABLE IF EXISTS second_table (id INT, name VARCHAR(256), score INT)) VALUES (1,2,3,4), ('John', 'Alex', 'Bob', 'Geogre'), (10,3,14,8);
+INSERT INTO IF EXISTS (CREATE TABLE IF EXISTS second_table (id INT, name VARCHAR(256), score INT)) VALUES (1,'John',10), (2, 'Alex', 3), (3,'Bob', 14), (4,'Geogre', 8);
