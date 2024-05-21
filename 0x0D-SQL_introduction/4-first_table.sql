1e78cd23fd43a7d59a8a8d8c23bf581dec3c4350
root <tasneemmidhat@gmail.com>
2024-05-21 10:24:11 -0700

    First

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- This command will create a table with two attributes id and name
-CREATE TABLE IF NOT EXISTS first_table (id INT), name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
