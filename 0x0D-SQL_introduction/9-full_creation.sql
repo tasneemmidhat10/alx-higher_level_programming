fad057063c99e1d1af1cf17c0195d7a3cbdbc9e4
root <tasneemmidhat@gmail.com>
2024-05-21 11:17:15 -0700

    First

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,10 @@
 -- Creating and inserting mutliple rows into a table
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT,
+       	name VARCHAR(256),
+       	score INT);
 INSERT INTO second_table 
-VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'Geogre', 8);
+VALUES 
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'Geogre', 8);
