27dbf26f0ea483003c1b4ba1646d641e32f639a9
root <tasneemmidhat@gmail.com>
2024-05-22 14:11:34 -0700

    First

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 CREATE DATABASE IF NOT EXISTS htbn_0d_usa;
 USE htbn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE PRIMARY KEY, name VARCHAR(256) IS NOT NULL);
+CREATE TABLE IF NOT EXISTS states (id INT PRIMARY KEY NOT NULL, AUTO_INCREMENT , name VARCHAR(256) IS NOT NULL);
