26dfc448b46f67682cfc83f5b65d037b5096aad2
root <tasneemmidhat@gmail.com>
2024-05-22 13:21:35 -0700

    First

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,2 @@
 --  script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-SHOW GRANTS user_0d_1, user_0d_2
+SHOW GRANTS for  user_0d_1@localhost, user_0d_2@localhost ;
